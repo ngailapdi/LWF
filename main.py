73f1c32ed5ab734c50ab88531d1bbba405599d61
Anh Thai <athai6@gatech.edu>
2018-10-18 01:45:32 -0400

    fix bug in main

--- main.py
+++ main.py
@@ -45,11 +45,12 @@ mean_image = np.load("cifar_mean_image.npy")
 
 total_classes = 100
 perm_id = np.random.permutation(total_classes)
-all_classes = np.arange(total_classes) 
+all_classes = np.arange(total_classes)
 for i in range(len(all_classes)):
 	all_classes[i] = perm_id[all_classes[i]]
 
 n_cl_temp = 0
+num_iters = total_classes//num_classes
 class_map = {}
 map_reverse = {}
 for i, cl in enumerate(all_classes):
@@ -60,7 +61,7 @@ for i, cl in enumerate(all_classes):
 print ("Class map:", class_map)
 
 for cl, map_cl in class_map.items():
-	map_reverse[map_cl] = int(cl) 
+	map_reverse[map_cl] = int(cl)
 
 print ("Map Reverse:", map_reverse)
 
@@ -71,7 +72,7 @@ print ("all_classes:", all_classes)
 with open(args.outfile, 'w') as file:
 	print("Classes, Train Accuracy, Test Accuracy", file=file)
 
-	
+
 	#shuffle classes
 	# random.shuffle(all_classes)
 	# class_map = {j: int(i) for i, j in enumerate(all_classes)}
@@ -86,7 +87,7 @@ with open(args.outfile, 'w') as file:
 	for s in range(0, num_iters, num_classes):
 		# Load Datasets
 		print('Iteration: ', s)
-		print('Algo running: ', args.algo)
+		#print('Algo running: ', args.algo)
 		print("Loading training examples for classes", all_classes[s: s+num_classes])
 		train_set = cifar100(root='./data',
 							 train=True,
@@ -107,7 +108,7 @@ with open(args.outfile, 'w') as file:
 												   shuffle=False, num_workers=12)
 
 		# Update representation via BackProp
-		model.update_representation(train_set, class_map, args, map_reverse, all_classes[s:s+num_classes], fine_tuning=False)
+		model.update(train_set, class_map, args, map_reverse, all_classes[s:s+num_classes])
 		model.eval()
 
 		model.n_known = model.n_classes
